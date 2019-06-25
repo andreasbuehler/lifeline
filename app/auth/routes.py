0a0b36346581f10c16a02259a8207003b0511405
Andreas Bühler <anbuehle@ethz.ch>
2019-06-25 18:50:30 +0200

    add logout

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -27,6 +27,12 @@ def login():
     return render_template('login.html', title=_('Sign In'), form=form)
 
 
+@bp.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
+
+
 @bp.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
